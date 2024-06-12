9beb142b2e3eed98eb3b4667e003ad888cbf7fbd
kelvin wepo <kelvinwepo7710@gmail.com>
2024-06-12 23:28:19 +0300

    this commit modifies users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile  # Import Profile model if it is in the same app
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)  # Add phone number field
     username = forms.CharField(
         label='Username',
         help_text=None  # Remove the informative text
@@ -22,4 +28,5 @@ class UserRegisterForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone_number', 'password1', 'password2']  # Include phone number
+
