883075fcf60af149b049be2150fed75ea061a67e
kelvin wepo <kelvinwepo7710@gmail.com>
2024-06-17 20:22:37 +0300

    ft:Added API views for Team, Player, Game, and Bet models

--- games/views.py
+++ games/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Team, Player, Game, Bet
+from .serializers import TeamSerializer, PlayerSerializer, GameSerializer, BetSerializer
 
-# Create your views here.
+
+class TeamAPIView(APIView):
+    def get(self, request):
+        teams = Team.objects.all()
+        serializer = TeamSerializer(teams, many=True)
+        return Response(serializer.data)
+
+class PlayerAPIView(APIView):
+    def get(self, request):
+        players = Player.objects.all()
+        serializer = PlayerSerializer(players, many=True)
+        return Response(serializer.data)
+
+class GameAPIView(APIView):
+    def get(self, request):
+        games = Game.objects.all()
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
+
+class BetAPIView(APIView):
+    def get(self, request):
+        bets = Bet.objects.all()
+        serializer = BetSerializer(bets, many=True)
+        return Response(serializer.data)
+ 
